50abced1b3596512e9ddb13e097636272debbf24
Develing1991 <completed0728@gmail.com>
2023-02-18 17:29:58 +0900

    feat: boardService createBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v4 as uuidV4 } from 'uuid';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuidV4(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
   }
 }
